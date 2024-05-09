7a0ef5ff9d48710e93127f6189f6c69587dce26d
SergioMartin86 <eienburuu@gmail.com>
2024-05-09 16:53:53 +0200

    Adding first version

---
+++ source/SMBCInstanceBase.hpp
@@ -0,0 +1,145 @@
+#pragma once
+
+#include <jaffarCommon/hash.hpp>
+#include <jaffarCommon/exceptions.hpp>
+#include <jaffarCommon/file.hpp>
+#include <jaffarCommon/serializers/base.hpp>
+#include <jaffarCommon/deserializers/base.hpp>
+#include <jaffarCommon/serializers/contiguous.hpp>
+#include <jaffarCommon/deserializers/contiguous.hpp>
+
+#include "controller.hpp"
+
+namespace stella
+{
+
+class EmuInstanceBase
+{
+  public:
+
+  EmuInstanceBase() = default;
+  virtual ~EmuInstanceBase() = default;
+
+  inline void advanceState(const std::string &move)
+  {
+    bool isInputValid = _controller.parseInputString(move);
+    if (isInputValid == false) JAFFAR_THROW_LOGIC("Move provided cannot be parsed: '%s'\n", move.c_str());
+
+    // Parsing power
+    if (_controller.getPowerButtonState() == true) JAFFAR_THROW_RUNTIME("Power button pressed, but not supported: '%s'\n", move.c_str());
+
+    // Parsing reset
+    if (_controller.getResetButtonState() == true) doSoftReset();
+
+    advanceStateImpl(_controller);
+  }
+
+  inline void setController1Type(const std::string& type)
+  {
+    bool isTypeRecognized = false;
+
+    if (type == "None") { _controller.setController1Type(Controller::controller_t::none); isTypeRecognized = true; }
+    if (type == "Gamepad") { _controller.setController1Type(Controller::controller_t::gamepad);  isTypeRecognized = true; }
+
+    if (isTypeRecognized == false) JAFFAR_THROW_LOGIC("Input type not recognized: '%s'\n", type.c_str());
+  }
+
+  inline void setController2Type(const std::string& type)
+  {
+    bool isTypeRecognized = false;
+
+    if (type == "None") { _controller.setController2Type(Controller::controller_t::none); isTypeRecognized = true; }
+    if (type == "Gamepad") { _controller.setController2Type(Controller::controller_t::gamepad);  isTypeRecognized = true; }
+    
+    if (isTypeRecognized == false) JAFFAR_THROW_LOGIC("Input type not recognized: '%s'\n", type.c_str());
+  }
+
+  inline jaffarCommon::hash::hash_t getStateHash() const
+  {
+    MetroHash128 hash;
+    
+    auto workRam = getWorkRamPointer();
+
+    //  Getting RAM pointer and size
+    hash.Update(workRam, 128);
+
+    jaffarCommon::hash::hash_t result;
+    hash.Finalize(reinterpret_cast<uint8_t *>(&result));
+    return result;
+  }
+
+  virtual void initialize() = 0;
+  virtual void initializeVideoOutput() = 0;
+  virtual void finalizeVideoOutput() = 0;
+  virtual void enableRendering() = 0;
+  virtual void disableRendering() = 0;
+
+  inline void loadROM(const std::string &romFilePath)
+  {
+    // Actually loading rom file
+    auto status = loadROMImpl(romFilePath);
+    if (status == false) JAFFAR_THROW_RUNTIME("Could not process ROM file");
+
+    _stateSize = getStateSizeImpl();
+    _differentialStateSize = getDifferentialStateSizeImpl();
+  }
+
+  void enableStateBlock(const std::string& block) 
+  {
+    enableStateBlockImpl(block);
+    _stateSize = getStateSizeImpl();
+    _differentialStateSize = getDifferentialStateSizeImpl();
+  }
+
+  void disableStateBlock(const std::string& block)
+  {
+     disableStateBlockImpl(block);
+    _stateSize = getStateSizeImpl();
+    _differentialStateSize = getDifferentialStateSizeImpl();
+  }
+
+  inline size_t getStateSize() const 
+  {
+    return _stateSize;
+  }
+
+  inline size_t getDifferentialStateSize() const
+  {
+    return _differentialStateSize;
+  }
+
+  // Virtual functions
+
+  virtual uint8_t* getWorkRamPointer() const = 0;
+  virtual void updateRenderer() = 0;
+  virtual void serializeState(jaffarCommon::serializer::Base& s) const = 0;
+  virtual void deserializeState(jaffarCommon::deserializer::Base& d) = 0;
+
+  virtual void doSoftReset() = 0;
+  virtual void doHardReset() = 0;
+  virtual std::string getCoreName() const = 0;
+
+  protected:
+
+  virtual bool loadROMImpl(const std::string &romData) = 0;
+  virtual void advanceStateImpl(const stella::Controller controller) = 0;
+
+  virtual void enableStateBlockImpl(const std::string& block) {};
+  virtual void disableStateBlockImpl(const std::string& block) {};
+
+  virtual size_t getStateSizeImpl() const = 0;
+  virtual size_t getDifferentialStateSizeImpl() const = 0;
+  
+  // State size
+  size_t _stateSize;
+
+  private:
+
+  // Controller class for input parsing
+  Controller _controller;
+
+  // Differential state size
+  size_t _differentialStateSize;
+};
+
+} // namespace stella
---
+++ source/new/SMBCInstance.hpp
@@ -0,0 +1,104 @@
+#pragma once
+
+#include "../SMBCInstanceBase.hpp"
+#include <string>
+#include <vector>
+#include <jaffarCommon/exceptions.hpp>
+#include <jaffarCommon/serializers/contiguous.hpp>
+#include <jaffarCommon/deserializers/contiguous.hpp>
+
+
+namespace smbc
+{
+
+class EmuInstance : public EmuInstanceBase
+{
+ public:
+
+ EmuInstance() : EmuInstanceBase()
+ {
+ }
+
+ ~EmuInstance()
+ {
+ }
+
+  virtual void initialize() override
+  {
+  }
+
+  virtual bool loadROMImpl(const std::string &romFilePath) override
+  {
+    return true;
+  }
+
+  void initializeVideoOutput() override
+  {
+  }
+
+  void finalizeVideoOutput() override
+  {
+  }
+
+  void enableRendering() override
+  {
+  }
+
+  void disableRendering() override
+  {
+  }
+
+  void serializeState(jaffarCommon::serializer::Base& s) const override
+  {
+  }
+
+  void deserializeState(jaffarCommon::deserializer::Base& d) override
+  {
+  }
+
+  size_t getStateSizeImpl() const override
+  {
+    return 0
+  }
+
+  uint8_t* getWorkRamPointer() const override
+  {
+    return nullptr;
+  }
+
+  void updateRenderer() override
+  {
+  }
+
+  inline size_t getDifferentialStateSizeImpl() const override { return getStateSizeImpl(); }
+
+  void enableStateBlockImpl(const std::string& block) override
+  {
+    JAFFAR_THROW_LOGIC("State block name: '%s' not found.", block.c_str());
+  }
+
+  void disableStateBlockImpl(const std::string& block) override
+  {
+    JAFFAR_THROW_LOGIC("State block name: '%s' not found", block.c_str());
+  }
+
+  void doSoftReset() override
+  {
+  }
+  
+  void doHardReset() override
+  {
+  }
+
+  std::string getCoreName() const override { return "New SMBC"; }
+
+
+  void advanceStateImpl(smbc::Controller controller) override
+  {
+  }
+
+  private:
+
+};
+
+} // namespace smbc
